6b8128e7dc214ada1ffb59c7e8854609d10caa4d
Aaron <gauntta@gmail.com>
2019-05-16 21:08:49 -0500

    Basic server / React template set up

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// const books = require("./routes/api-routes");
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Connect to mongoose
+mongoose
+    .connect(process.env.MONGODB_URI || "mongodb://localhost/IronHaus", { useNewUrlParser: true })
+    .then(() => console.log("MongoDB connected..."))
+    .catch(err => console.log(err));
+
+// Use Routes
+// app.use("/api/books", books);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
