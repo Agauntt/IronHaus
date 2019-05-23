cefec923fbccd0a08ebdf59a33b29ad754b7d1ea
Aaron <gauntta@gmail.com>
2019-05-22 22:20:27 -0500

    EOD - barebones post component built

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import '../Style.css';
+import Post from './Post';
 
 class Feed extends Component {
     state = {
-        Posts: "This is a post"
+        Posts: "",
+        Workouts: []
     }
+
+
 
     render() {
         return (
-            <div className='feed'>
-                
+            <div className='feed container'>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
             </div>
         )
     }
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 
-export default class Post extends Component {
+class Post extends Component {
+    state = {
+        exercise: "bench press",
+        sets: '2',
+        weight: '185'
+    }
+
     render() {
         return (
-            <div>
-                
+            <div style={{borderBottom: 'solid', borderWidth: '1px'}}>
+                <div className='row'>
+                    <div className='col-3'>
+                        <h3 style={{textDecoration: 'underline'}}>Poster Name</h3>
+                    </div>
+                    <div className='col-9'>
+                        <h5>{this.state.exercise}</h5>
+                        <li style={{marginLeft: '15px'}}>Sets: {this.state.sets}</li>
+                        <li style={{marginLeft: '15px'}}>Weight: {this.state.weight}</li>
+                    </div>    
+                </div>
             </div>
         )
     }
 }
+
+export default Post
