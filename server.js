37126642605474d052ad32970c072316a0729a0d
Aaron <gauntta@gmail.com>
2019-05-30 14:09:23 -0500

    lets try this again...

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ require('./config/passport')(passport);
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -41,6 +33,15 @@ mongoose
 // Use Routes
 app.use('/api', users);
 app.use('/api', workouts);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 
