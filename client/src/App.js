e9d42f0253ea95186219e4aee920dae751542f09
Aaron <gauntta@gmail.com>
2019-05-30 15:35:36 -0500

    adding switch to routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Style.css";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import SignUp from "./Pages/Sign-up";
 import Home from "./Pages/Home";
@@ -11,9 +11,11 @@ function App() {
   return (
     <Router>
       <div className="wrapper" >
+        <Switch>
         <Route path="/" exact={true} component={Home} />
         <Route path="/profile" exact={false} component={Profile} />
         <Route path="/Sign-up" exact={true} component={SignUp} />
+        </Switch>
       </div>
     </Router>
   );
