833fbf605ffe7e25c50c87a7710d466894cded9d
Aaron <gauntta@gmail.com>
2019-05-30 15:53:01 -0500

    removing build folder from gitignore

---
+++ client/build/precache-manifest.cc1915eb66292455427376958e974fe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf770be9bfbe297832e1c17f2811e8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a0118dcf5acfb94011a",
+    "url": "/static/css/main.f2306a24.chunk.css"
+  },
+  {
+    "revision": "0494e561feb6de3ad53e",
+    "url": "/static/js/2.29599b98.chunk.js"
+  },
+  {
+    "revision": "8a0118dcf5acfb94011a",
+    "url": "/static/js/main.8a219753.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f96ff76146a8e97c434124d5fdf2e728",
+    "url": "/static/media/IronHausLogo.f96ff761.png"
+  },
+  {
+    "revision": "1041b40c95d2c26ba1a7d0f356cbb8c9",
+    "url": "/static/media/male1.1041b40c.jpg"
+  }
+]);
