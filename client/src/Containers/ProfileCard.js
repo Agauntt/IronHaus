b1527a1322852af13f5d346e6cb795575a962239
Aaron <gauntta@gmail.com>
2019-05-31 10:00:56 -0500

    Adjusting profile card for mobile viewing

--- client/src/Containers/ProfileCard.js
+++ client/src/Containers/ProfileCard.js
@@ -6,16 +6,33 @@ class ProfileCard extends React.Component {
   render() {
     const { name, weight, height, age, goals } = this.props;
     return (
-      <div style={{ display: "inline-block" }}>
-        <div className="profile-card" style={{ width: "16rem" }}>
-          <div className="card-img-top" style={{ textAlign: "center" }}>
-            <img src={avatar} alt="avatar" className="avatar" />
+      //   <div className="profile-card">
+      //     <div className="card-img-top" style={{ textAlign: "center" }}>
+      //       <img src={avatar} alt="avatar" className="avatar" />
+      //     </div>
+      //     <div className="card-img-left" style={{ textAlign: "center" }}>
+      //       <img src={avatar} alt="avatar" className="avatar" />
+      //     </div>
+      //     <div className="card-body">
+      //       <div className="card-title" style={{ textAlign: "center" }}>
+      //         {name}{" "}
+      //       </div>
+      //       <div>
+      //         <li>Current weight: {weight} lbs</li>
+      //         <li>Height: {height}''</li>
+      //         <li>Age: {age} yrs </li>
+      //         <li>Fitness Goals: {goals} </li>
+      //       </div>
+      //     </div>
+      //   </div>
+      <div class="profile-card">
+        <div class="card flex-row flex-wrap">
+          <div class="card-header">
+            <img src={avatar} className="avatar" alt="" />
           </div>
-          <div className="card-body">
-            <div className="card-title" style={{ textAlign: "center" }}>
-              {name}{" "}
-            </div>
-            <div>
+          <div class="card-block px-2">
+            <h4 class="card-title">{name}</h4>
+            <div class="card-text w-90">
               <li>Current weight: {weight} lbs</li>
               <li>Height: {height}''</li>
               <li>Age: {age} yrs </li>
--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Axios from 'axios';
-import NavBar from '../Containers/NavBar';
-import ProfileCard from '../Containers/ProfileCard';
-import Feed from '../Containers/Feed';
-import NewWorkout from '../Containers/NewWorkout'
+import Axios from "axios";
+import NavBar from "../Containers/NavBar";
+import ProfileCard from "../Containers/ProfileCard";
+import Feed from "../Containers/Feed";
+import NewWorkout from "../Containers/NewWorkout";
 
 class Profile extends React.Component {
   state = {
@@ -15,20 +15,20 @@ class Profile extends React.Component {
     age: "N/A",
     goals: "N/A",
     step: 1
-  }
+  };
 
   componentDidMount = () => {
     const url = window.location.pathname;
-    const userID = url.replace("/profile/id=", '').replace("%7D", '');
+    const userID = url.replace("/profile/id=", "").replace("%7D", "");
 
     this.setState({
       id: userID
     });
-      this.pullUser(userID)
-  }
+    this.pullUser(userID);
+  };
 
   pullUser = async e => {
-    let res = await Axios.get('/api/users/' + e)
+    let res = await Axios.get("/api/users/" + e);
     let { data } = await res;
     this.setState({
       name: data.name,
@@ -37,18 +37,18 @@ class Profile extends React.Component {
       age: data.age,
       goals: data.goals,
       email: data.email
-    })
-  }
+    });
+  };
 
   handleNewWorkout = () => {
-    if(this.state.step === 1){
+    if (this.state.step === 1) {
       this.setState({
         step: 2
-      })
+      });
     } else {
       this.setState({
         step: 1
-      })
+      });
     }
   };
 
@@ -56,71 +56,61 @@ class Profile extends React.Component {
     const { step } = this.state;
     switch (step) {
       case 1:
-      return (
-        <div>
-          <NavBar 
-          handleNewWorkout={this.handleNewWorkout}
-          />
-          <div className="row body-box" style={{textAlign: 'center', display: 'flex'}}>
-          <div className="sm-col-12">
-          <ProfileCard 
-          name= {this.state.name}
-          height= { this.state.height } 
-          weight= { this.state.weight } 
-          age= { this.state.age } 
-          goals= { this.state.goals } 
-          /> 
-          </div>
-        <Feed />
-        </div>
-      </div>
-    );
-      case 2:
-    return (
-      <div>
-        <NavBar 
-          name={ this.state.name }
-          handleNewWorkout={this.handleNewWorkout}
-          />
-          <div style={{display: 'flex'}}>
-          <ProfileCard 
-          name= {this.state.name}
-          height= { this.state.height } 
-          weight= { this.state.weight } 
-          age= { this.state.age } 
-          goals= { this.state.goals } 
-          /> 
-          <NewWorkout 
-          name={this.state.name}
-          email={this.state.email}
-          closePost={this.handleNewWorkout}/>
-
-        </div>
-      </div>
-    );
-      default:
-        return(
+        return (
           <div>
-          <NavBar 
-          handleNewWorkout={this.handleNewWorkout}
-          />
-          <div className="row body-box" style={{textAlign: 'center', display: 'flex'}}>
-          <div className="sm-col-12">
-          <ProfileCard 
-          name= {this.state.name}
-          height= { this.state.height } 
-          weight= { this.state.weight } 
-          age= { this.state.age } 
-          goals= { this.state.goals } 
-          /> 
-          </div> 
-        <Feed />
-         </div>
-      </div>
-        )
-      }
+            <NavBar handleNewWorkout={this.handleNewWorkout} />
+            <div className="row body-box" style={{ textAlign: "center", display: "flex" }}>
+              <ProfileCard
+                name={this.state.name}
+                height={this.state.height}
+                weight={this.state.weight}
+                age={this.state.age}
+                goals={this.state.goals}
+              />
+              <Feed />
+            </div>
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <NavBar name={this.state.name} handleNewWorkout={this.handleNewWorkout} />
+            <div style={{ display: "flex" }}>
+              <ProfileCard
+                name={this.state.name}
+                height={this.state.height}
+                weight={this.state.weight}
+                age={this.state.age}
+                goals={this.state.goals}
+              />
+              <NewWorkout
+                name={this.state.name}
+                email={this.state.email}
+                closePost={this.handleNewWorkout}
+              />
+            </div>
+          </div>
+        );
+      default:
+        return (
+          <div>
+            <NavBar handleNewWorkout={this.handleNewWorkout} />
+            <div className="row body-box" style={{ textAlign: "center", display: "flex" }}>
+              <div className="sm-col-12">
+                <ProfileCard
+                  name={this.state.name}
+                  height={this.state.height}
+                  weight={this.state.weight}
+                  age={this.state.age}
+                  goals={this.state.goals}
+                />
+              </div>
+              <Feed />
+            </div>
+          </div>
+        );
     }
   }
-
+}
 
 export default Profile;
